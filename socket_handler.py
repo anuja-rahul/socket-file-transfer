08f0a0c5140af94b9a3447f02110515117667806
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-02-14 20:42:46 +0530

    added SocketHandler class

---
+++ socket_handler.py
@@ -0,0 +1,10 @@
+"""
+socket_file_transfer/socket_handler.py
+Main script for accessing the client and server classes.
+"""
+
+from abc import ABCMeta, abstractmethod
+
+
+class SocketHandler(metaclass=ABCMeta):
+    pass
