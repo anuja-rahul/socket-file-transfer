edd6303df16c268068cdea3717136ea930156d03
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-02-14 22:47:46 +0530

    added SocketHandler singleton method

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ socket_file_transfer/main.py
 Main script for accessing the handler class.
 """
 
-from socket_handler import SocketHandler
+from socket_handler import SocketSender
 
-instance = SocketHandler()
+instance = SocketSender(password="TestPassword1234", nonce="TestNonce1234567")
