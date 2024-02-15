e750f8b882f4eb4c4ea1e22ea88573152e734c9f
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-02-15 21:38:47 +0530

    connected to validation methods.

--- socket_client.py
+++ socket_client.py
@@ -5,10 +5,13 @@ Main script for accessing the client classes.
 
 from abc import ABCMeta, abstractmethod, ABC
 
+from validator import Validator
+from init_handler import InitEnv
+
 
 class ISocketClient(metaclass=ABCMeta):
     @abstractmethod
-    def get_data(self):
+    def print_data(self):
         """Implemented in child class"""
 
 
@@ -18,13 +21,32 @@ class SocketClient(ISocketClient, ABC):
     @staticmethod
     def get_instance():
         if SocketClient.__instance is None:
-            SocketClient(password="root", nonce="nonce")
+            SocketClient(password=b"SocketClientsPWD", nonce=b"SocketClientsNCE", file="test.txt")
         return SocketClient.__instance
 
-    def __init__(self, password, nonce, send: bool = False):
+    def __init__(self, file: str, password: bytes, nonce: bytes, send: bool = False):
+        InitEnv.init_env()
+
         if SocketClient.__instance is not None:
             raise Exception("Cannot be instantiated more than once")
+        else:
+            self.__password = password
+            self.__nonce = nonce
+            self.__send = send
+            self.__file = file
+            self.__validator = Validator(password=password, nonce=nonce, file=file)
+            self.__hashes = self.__validator.get_hashes
 
-        self.__password = password
-        self.__nonce = nonce
-        self.__send = send
+            if self.__validator.validate_length():
+                self.__validity = True
+            else:
+                raise Exception("\nPassword and Nonce must be exactly 16 characters long !\n")
+
+            SocketClient.__instance = self
+
+    @staticmethod
+    def print_data(**kwargs):
+        print(f"({SocketClient.__instance.send})")
+
+    def __repr__(self):
+        return f"({self.__password}, {self.__nonce}, {self.__send})"
