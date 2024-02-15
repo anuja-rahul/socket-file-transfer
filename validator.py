7890e8230a93e392296a72de44693fae6c54d751
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-02-15 21:38:24 +0530

    added hashing and length validation methods.

---
+++ validator.py
@@ -0,0 +1,38 @@
+"""
+socket_file_transfer/validator.py
+Main script for validating data.
+"""
+
+import uuid
+import hashlib
+
+
+class Validator:
+    def __init__(self, password: bytes = None, nonce: bytes = None, file: str = None):
+        self.__request = uuid.uuid4()
+        self.__password = password
+        self.__nonce = nonce
+        self.file = file
+
+    def validate_length(self):
+        if self.__password and self.__nonce:
+            if len(self.__password) != 16 or len(self.__nonce) != 16:
+                return False
+            else:
+                return True
+
+    # noinspection PyTypeChecker
+    def get_hashes(self):
+        pwd_hash = None
+        nonce_hash = None
+        file_hash = None
+
+        if self.__password and self.__nonce:
+            pwd_hash = hashlib.sha256(self.__password)
+            nonce_hash = hashlib.sha256(self.__nonce)
+
+        if self.file:
+            with open(self.file, 'rb') as file:
+                file_hash = hashlib.file_digest(file, "sha256").hexdigest()
+
+        return {"password_hash": pwd_hash, "nonce_hash": nonce_hash, "file_hash": file_hash}
