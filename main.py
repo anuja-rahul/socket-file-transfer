6d8dfa5f031c4d8a762ff24b1319581c7e39748c
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-02-15 21:37:58 +0530

    minor changes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ socket_file_transfer/main.py
 Main script for accessing the handler class.
 """
 
-from socket_client import SocketSender
+from socket_client import SocketClient
 
-instance = SocketSender(password="TestPassword1234", nonce="TestNonce1234567")
+instance = SocketClient(password=b"TestPassword1234", nonce=b"TestNonce1234567", send=True, file="test.txt")
+print(instance)
