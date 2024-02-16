a3b08cce62e1a225ec59381646902a4ae48d5643
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-02-16 11:39:06 +0530

    testing and debugging

---
+++ receive.py
@@ -0,0 +1,11 @@
+"""
+socket_file_transfer/receive.py
+Main script handling receiving data.
+"""
+
+from socket_server import SocketServer
+
+receive_instance = SocketServer(key=b"TestPassword1234", nonce=b"TestNonce1234567", receive=True, file="test.txt")
+print(receive_instance)
+receive_instance.print_data()
+receive_instance.receive_data()
---
+++ send.py
@@ -0,0 +1,12 @@
+"""
+socket_file_transfer/send.py
+Main script handling sending data.
+"""
+
+from socket_client import SocketClient
+
+
+send_instance = SocketClient(key=b"TestPassword1234", nonce=b"TestNonce1234567", send=True, file="test.txt")
+print(send_instance)
+send_instance.print_data()
+send_instance.send_data()
