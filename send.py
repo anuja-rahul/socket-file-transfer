c1b890882414f31ab88c1e01a70a2e2dccdd54cc
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-02-17 01:10:55 +0530

    bug testing

--- send.py
+++ send.py
@@ -7,6 +7,6 @@ from socket_client import SocketClient
 
 
 send_instance = SocketClient(key=b"TestPassword1234", nonce=b"TestNonce1234567", send=True, file="vid.mp4")
-print(send_instance)
-send_instance.print_data()
+# print(send_instance)
+# send_instance.print_data()
 send_instance.send_data()
