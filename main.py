7c600a3afcfc15f8ec95c8b6b5c34b1a9a0a6a53
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-02-16 10:59:47 +0530

    testing and debugging

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ Main script for accessing the handler class.
 """
 
 from socket_client import SocketClient
+from socket_server import SocketServer
 
-instance = SocketClient(key=b"TestPassword1234", nonce=b"TestNonce1234567", send=True, file="test.txt")
-print(instance)
-instance.print_data()
+receive_instance = SocketServer(key=b"TestPassword1234", nonce=b"TestNonce1234567", receive=True, file="test.txt")
+print(receive_instance)
+receive_instance.print_data()
+
+send_instance = SocketClient(key=b"TestPassword1234", nonce=b"TestNonce1234567", send=True, file="test.txt")
+print(send_instance)
+send_instance.print_data()
