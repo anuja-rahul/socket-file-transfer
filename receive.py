fa6dd1d7bf7d0d640bb998d8d327bcae43a0e5e8
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-02-16 12:17:52 +0530

    testing and debugging

--- receive.py
+++ receive.py
@@ -5,7 +5,7 @@ Main script handling receiving data.
 
 from socket_server import SocketServer
 
-receive_instance = SocketServer(key=b"TestPassword1234", nonce=b"TestNonce1234567", receive=True, file="test.txt")
+receive_instance = SocketServer(key=b"TestPassword1234", nonce=b"TestNonce1234567", receive=True, file="language.pdf")
 print(receive_instance)
 receive_instance.print_data()
 receive_instance.receive_data()
